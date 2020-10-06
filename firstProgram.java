4848fd170b14f10815b3be636400314dc58cc0c8
Anushree176 <anushree.kolhe18@siesgst.ac.in>
2020-10-06 12:43:06 +0530

    Added programs for Java and Python

---
+++ firstProgram.java
@@ -0,0 +1,17 @@
+//Hello World Program
+//Filename must be same as the class name, here "firstProgram"
+class firstProgram {
+    //Program starts with a call to main function
+    public static void main(String[] args) {
+        //println is an in-built function in System class
+        System.out.println("Hello, World!"); 
+    }
+}
+/*How to run Java program:
+    Compile - javac firstProgram.java 
+    Execute - java firstProgram
+
+References to learn Java
+    https://www.tutorialspoint.com/java/index.htm
+    https://www.geeksforgeeks.org/java/ 
+*/
---
+++ hello_world.py
@@ -0,0 +1,11 @@
+# For Python 2
+# Here print is not considered as a function
+print "Hello World!"
+
+# For Python 3
+# Here print is a function - paranthesis used
+print("Hello World!")
+
+# References to learn Python
+# https://www.learnpython.org/
+# https://www.programiz.com/python-programming
